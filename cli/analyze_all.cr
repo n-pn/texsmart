cf0dbdf294707717ccf738bc03a9706bc78b7737
Nipinium <nipinium@gmail.com>
2023-02-16 20:09:37 +0700

    update analyze_all script

--- cli/analyze_all.cr
+++ cli/analyze_all.cr
@@ -1,18 +1,12 @@
+require "colorize"
 require "../src/texsmart"
 
-LIMIT   = ENV["LIMIT"]?.try(&.to_i) || 256
 WORKERS = ENV["WORKERS"]?.try(&.to_i?) || 4
-
-MOD = ENV["MOD"]?.try(&.to_i) || 1
-REM = ENV["REM"]?.try(&.to_i) || 0
 
 puts "Starting NLU engine with worker_count: #{WORKERS}"
 ENGINE = Texsmart.new(worker_count: WORKERS)
 
 puts "Engine loading completed, parsing!"
-
-INP_DIR = "/app/chivi/var/texts/rgbks"
-OUT_DIR = "/mnt/devel/Chivi/analyze"
 
 def write_terms(file : File, terms : Enumerable(Texsmart::Term))
   terms.each do |term|
@@ -32,75 +26,96 @@ def write_entities(file : File, entities : Enumerable(Texsmart::Entity))
   file << '\n'
 end
 
-def analyze_file(file : String, out_file = file.sub(INP_DIR, OUT_DIR))
-  puts "- Parsing: #{file.sub(INP_DIR, "")}"
+def analyze_file(inp_file : String, out_base : String)
+  lines = File.read_lines(inp_file, encoding: "GB18030")
+  c_len = lines.sum(&.size)
 
-  return if File.file?(out_file)
+  words_path = "#{out_base}.word_log[M].tsv"
+  phrases_path = "#{out_base}.phrase_log[M].tsv"
+  entities_path = "#{out_base}.entity_fine[M].tsv"
 
-  words_path = out_file.sub(".gbk", "-words.tsv")
+  inp_base = File.basename(inp_file)
 
-  phrases_path = out_file.sub(".gbk", "-phrases.tsv")
-  entities_path = out_file.sub(".gbk", "-entities.tsv")
+  if info = File.info?(words_path)
+    puts "-- #{inp_base} analyzed, skipping!".colorize.blue
+    return {c_len, info.modification_time.to_unix}
+  else
+    puts "-- Parsing: #{inp_base}, words: #{c_len}".colorize.green
+  end
 
   out_words = File.open(words_path, "w")
   out_phrases = File.open(phrases_path, "w")
-  out_entities = File.open(entities_path.sub(".gbk", "-entities.tsv"), "w")
+  out_entities = File.open(entities_path, "w")
 
-  File.each_line(file, encoding: "GB18030") do |line|
+  lines.each do |line|
     next if line.empty?
     words, phrases, entities = ENGINE.parse(line)
+
     write_terms(out_words, words)
     write_terms(out_phrases, phrases)
     write_entities(out_entities, entities)
-    out_entities << line << '\n'
   end
 
   out_words.close
   out_phrases.close
   out_entities.close
 
-  File.touch(out_file)
+  {c_len, Time.utc.to_unix}
 end
 
-# def analyze_glob(glob : String)
-#   glob = File.join(INP_DIR, glob) unless glob.starts_with?(INP_DIR)
-#   files = Dir.glob("#{glob}/**/*.gbk")
+def analyze_book(idx_path : String) : Nil
+  out_dir = "#{OUT_DIR}/#{File.basename(idx_path, ".tsv")}"
+  Dir.mkdir_p(out_dir)
 
-#   puts "- extract #{glob.sub(INP_DIR, "")}: #{files.size} files"
+  input = {} of String => Array(String)
 
-#   files.map { |x| File.dirname(x) }.uniq!.map { |x| Dir.mkdir_p(x) }
-#   files.each { |file| analyze_file(file) }
-# end
-
-# ARGV.each do |glob|
-#   next if glob.starts_with?('-')
-#   analyze_glob(glob)
-# end
-
-def analyze_seed(seed : String, limit = 256, mod = 1, rem = 0)
-  s_dir = "#{INP_DIR}/#{seed}"
-  b_ids = Dir.children(s_dir).map(&.to_i).sort!
-
-  b_ids.each do |s_bid|
-    Dir.mkdir_p("#{OUT_DIR}/#{seed}/#{s_bid}")
-
-    files = Dir.glob("#{s_dir}/#{s_bid}/*.gbk")
-
-    if limit > 0
-      files.sort_by! { |x| File.basename(x, ".gbk").to_i }
-      files = files.first(limit)
-    end
-
-    puts "- extract #{seed}/#{s_bid}: #{files.size} files"
-    time = Time.measure do
-      files.each_with_index { |file, i| analyze_file(file) if i % mod == rem }
-    end
-
-    puts " done in: #{time.total_seconds.round(2)} seconds"
+  File.each_line(idx_path) do |line|
+    rows = line.split('\t')
+    next unless ch_no = rows.first?
+    input[ch_no] = rows
   end
+
+  puts "- analyzing #{out_dir}, entries: #{input.size}".colorize.yellow
+
+  start = Time.monotonic
+
+  input.each_value do |rows|
+    next if rows.size > 2 # skip analyzed
+
+    ch_no, tpath = rows
+    sname = tpath.split('/', 2)[0]
+
+    inp_path = "#{TXT_DIR}/#{tpath}.gbk"
+    out_base = "#{out_dir}/#{ch_no}-[#{sname}]"
+
+    c_len, mtime = analyze_file(inp_path, out_base)
+    rows << c_len.to_s << mtime.to_s
+
+    File.open(idx_path, "a", &.puts(rows.join('\t')))
+  rescue ex
+    puts ex.colorize.red
+  end
+
+  tdiff = Time.monotonic - start
+  puts "Done in: #{tdiff.total_seconds.round(2)} seconds".colorize.cyan
 end
 
-ARGV.each do |seed|
-  next if seed.starts_with?('-')
-  analyze_seed(seed, limit: LIMIT, mod: MOD, rem: REM)
+INP_DIR = "/app/chivi/var/cvmtl/index"
+TXT_DIR = "/app/chivi/var/texts/rgbks"
+OUT_DIR = "/app/chivi/var/texts/anlzs"
+
+mod = ENV["MOD"]?.try(&.to_i) || 1
+rem = ENV["REM"]?.try(&.to_i) || 0
+
+files = Dir.glob("#{INP_DIR}/*.tsv").map do |file|
+  {file, File.basename(file).split('-', 2).first.to_i}
+end
+
+files = files.select! { |_, id| id % mod == rem } if mod > 1
+files.sort_by!(&.[1])
+
+puts "input: #{files.size}"
+
+files.each do |file, _|
+  analyze_book(file)
 end
